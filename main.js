04601fa9c24a47677a53f7aeff91da0de53ab18d
New Joe <new.joseph10@gmail.com>
2013-06-17 16:51:36 -0400

    Added more

--- main.js
+++ main.js
@@ -1,6 +1,36 @@
+// Copyright shrimpboyho.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+
 // Require all the stuff needed
 
 var http = require('http');
+var fileManager = require('fs');
+
+
+// Load up all the stuff
+
+var htmlPage = fileManager.readFileSync("index.html");
+
 
 // Create the server
 
@@ -10,8 +40,33 @@ var server = http.createServer();
 
 server.on('request',function(request,response){
     
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World from Cloud10\n');
+    // Write initial stuff to the client
+    
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    
+    // Serve the htmlfile
+    
+    response.write(htmlPage);
+    
+    // Read data sent to the server
+    
+    request.on('data',function(chunk){
+        
+        console.log(chunk.toString());
+        
+    });
+    
+    // When there is no more data left to read
+    
+    request.on('end',function(){      
+        
+        // End the response
+    
+        response.end();
+           
+    });
+    
+
     
 });
 
